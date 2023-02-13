5e1da4ee307d270a0c97c035016027d1f27268d1
Viking Berglind <viking.berglind@kan.se>
2023-02-13 11:00:33 +0100

    push to see if it floats you boat

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,93 +10,92 @@ import Instruments from "../Instruments";
 import { pingpong } from "three/src/math/MathUtils";
 
 const synth = new Tone.Synth({
-  "harmonicity":8,
-  "modulationIndex": 2,
-  "oscillator" : {
-      "type": "sine"
+  harmonicity: 8,
+  modulationIndex: 2,
+  oscillator: {
+    type: "sine",
   },
-  "envelope": {
-      "attack": 0.001,
-      "decay": 2,
-      "sustain": 0.1,
-      "release": 2
+  envelope: {
+    attack: 0.001,
+    decay: 2,
+    sustain: 0.1,
+    release: 2,
   },
-  "modulation" : {
-      "type" : "square"
+  modulation: {
+    type: "square",
   },
-  "modulationEnvelope" : {
-      "attack": 0.002,
-      "decay": 0.2,
-      "sustain": 0,
-      "release": 0.2
-  }
+  modulationEnvelope: {
+    attack: 0.002,
+    decay: 0.2,
+    sustain: 0,
+    release: 0.2,
+  },
 }).toDestination();
 const synth2 = new Tone.Synth({
-
-  "harmonicity":8,
-  "modulationIndex": 2,
-  "oscillator" : {
-      "type": "sine"
+  harmonicity: 8,
+  modulationIndex: 2,
+  oscillator: {
+    type: "sine",
   },
-  "envelope": {
-      "attack": 0.001,
-      "decay": 2,
-      "sustain": 0.1,
-      "release": 2
+  envelope: {
+    attack: 0.001,
+    decay: 2,
+    sustain: 0.1,
+    release: 2,
   },
-  "modulation" : {
-      "type" : "square"
+  modulation: {
+    type: "square",
   },
-  "modulationEnvelope" : {
-      "attack": 0.002,
-      "decay": 0.2,
-      "sustain": 0,
-      "release": 0.2
-  }
+  modulationEnvelope: {
+    attack: 0.002,
+    decay: 0.2,
+    sustain: 0,
+    release: 0.2,
+  },
 }).toDestination();
 const synth3 = new Tone.Synth({
-  "harmonicity":8,
-  "modulationIndex": 2,
-  "oscillator" : {
-      "type": "sine"
+  harmonicity: 8,
+  modulationIndex: 2,
+  oscillator: {
+    type: "sine",
   },
-  "envelope": {
-      "attack": 0.001,
-      "decay": 2,
-      "sustain": 0.1,
-      "release": 2
+  envelope: {
+    attack: 0.001,
+    decay: 2,
+    sustain: 0.1,
+    release: 2,
   },
-  "modulation" : {
-      "type" : "square"
+  modulation: {
+    type: "square",
   },
-  "modulationEnvelope" : {
-      "attack": 0.002,
-      "decay": 0.2,
-      "sustain": 0,
-      "release": 0.2
-  }
+  modulationEnvelope: {
+    attack: 0.002,
+    decay: 0.2,
+    sustain: 0,
+    release: 0.2,
+  },
 }).toDestination();
 const synth4 = new Tone.Synth({
-  "harmonicity":8,
-  "modulationIndex": 2,
-  "oscillator" : {
-      "type": "sine"
+  harmonicity: 8,
+  modulationIndex: 2,
+  oscillator: {
+    type: "sine",
   },
-  "envelope": {
-      "attack": 0.001,
-      "decay": 2,
-      "sustain": 0.1,
-      "release": 2
+  envelope: {
+    attack: 0.001,
+    decay: 2,
+    sustain: 0.1,
+    release: 2,
   },
-  "modulation" : {
-      "type" : "square"
+  modulation: {
+    type: "square",
   },
-  "modulationEnvelope" : {
-      "attack": 0.002,
-      "decay": 0.2,
-      "sustain": 0,
-      "release": 0.2
-  }
+  modulationEnvelope: {
+    attack: 0.002,
+    decay: 0.2,
+    sustain: 0,
+    release: 0.2,
+  },
 }).toDestination();
 
 const synths = [synth, synth2, synth3, synth4];
@@ -126,23 +125,18 @@ const IDs = [
   Math.random(),
   Math.random(),
   Math.random(),
-]
+];
 
-
-const pingPong = new Tone.PingPongDelay("4n", 0.2)
-
-
+const pingPong = new Tone.PingPongDelay("4n", 0.2);
 function App() {
+  console.log('rerender')
   const [isPlaying, setIsPlaying] = React.useState(false);
   const [allSeries, setAllSeries] = React.useState({});
   const [delay, setDelay] = React.useState(0);
   const [step, setStep] = React.useState(-1);
 
-
-
-
   function startLoop() {
-    if(isPlaying) return;
+    if (isPlaying) return;
     Tone.start();
     Tone.Transport.start();
     pingPong.toDestination();
@@ -162,38 +156,37 @@ function App() {
     Tone.Transport.bpm.value = 90;
     const loop = new Tone.Loop((time) => {
       // triggered every eighth note.
+      // playSeries();
       setStep((current) => current + 1);
-      playSeries();
     }, "8n").start(0);
   }
 
   function playSeries() {
-    if(!isPlaying) return;
+    if (!isPlaying) return;
+    if (
+      Object.keys(allSeries).length === 0 &&
+      allSeries.constructor === Object
+    ) {
+      return;
+    }
     Tone.Transport.start();
     const now = Tone.now();
-    // allSeries.map((series, i) => {
-      //   console.log(series)
-      // })
-      
-      
-      
-      if(Object.keys(allSeries).length === 0 && allSeries.constructor === Object) {
-        console.log('empty')
-        return
+
+    pingPong.wet.value = delay;
+
+    let keys = Object.keys(allSeries);
+    for (let key in keys) {
+      let instrument = allSeries[keys[key]];
+
+      if (instrument.series[step % 16]?.pressed) {
+        // synths[key].triggerAttackRelease(instrument.series[step % 16].note, "4n", now + key);
+        synths[key].triggerAttackRelease(
+          instrument.series[step % 16].note,
+          "4n",
+          now
+        );
       }
-      pingPong.wet.value = delay ;
-      
-      let keys = Object.keys(allSeries);
-      for(let key in keys){
-        let instrument = allSeries[keys[key]];
-
-        if(instrument.series[step % 16]?.pressed){
-          // synths[key].triggerAttackRelease(instrument.series[step % 16].note, "4n", now + key);
-          synths[key].triggerAttackRelease(instrument.series[step % 16].note, "4n", now);
-        }
-
-
-      }
+    }
   }
 
   React.useEffect(() => {
@@ -202,19 +195,21 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Header>{isPlaying == true 
-        ? (step % 16) + 1
-        : '<Sequenzio />'
-     }</Header>
+      <Header>{isPlaying == true ? (step % 16) + 1 : "<Sequenzio />"}</Header>
 
       <div className="sequencer">
         <StepIndicator step={step % 16} delay={delay} setDelay={setDelay} />
-        <Instruments IDs={IDs} step={step % 16} allSeries={allSeries} setAllSeries={setAllSeries}  />
+        <Instruments
+          IDs={IDs}
+          step={step % 16}
+          allSeries={allSeries}
+          setAllSeries={setAllSeries}
+        />
         <div className="row-right">
           <button onClick={startLoop}>play me</button>
           <button onClick={stopLoop}>stop me</button>
         </div>
-      {/* <CanvasApp step={step} /> */}
+        {/* <CanvasApp step={step} /> */}
       </div>
     </div>
   );
